bbb70f26074ed2e79cc383c96e9ee6630ad6ae93
Nikita <grinko1999@mail.ru>
2020-10-12 17:43:47 +0500

    finish

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const getPizzas = (category, sortBy) => {
     return axios
-        .get(`http://localhost:3001/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`)
+        .get(`/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`)
 
 }
